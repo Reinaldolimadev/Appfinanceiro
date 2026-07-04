5722959d11d3619d1ebaf0cea18668e504125ab4
Reinaldo Machado Lima <reinaldolimadev@gmail.com>
2026-07-04 15:28:54 -0300

    adiciona componente Button e configuração inicial 'wq'

---
+++ src/router.tsx
@@ -0,0 +1,31 @@
+import { PiggyBank } from 'lucide-react'
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Button } from './components/shared/button'
+
+export const router = createBrowserRouter([
+  {
+    children: [
+      {
+        path: '/',
+   element: (
+  <div>
+    <h1>Formulário de Simulação</h1>
+
+    <Button variant="primary" icon={PiggyBank}>
+      Clique aqui
+    </Button>
+  </div>
+),
+      },
+      {
+        path: '/resultado/:id',
+        element: <h1>Resultado da Simulação</h1>,
+      },
+      {
+        path: '/historico',
+        element: <h1>Histórico de Simulações</h1>,
+      },
+    ],
+  },
+])
