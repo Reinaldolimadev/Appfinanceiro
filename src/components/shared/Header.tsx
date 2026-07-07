20ffc104ee918c1f397ac44de1a9c1437e73ba87
Reinaldo Machado Lima <reinaldolimadev@gmail.com>
2026-07-07 16:12:10 -0300

    modo light & Dark

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,9 +1,14 @@
-import { Clock, Landmark, TrendingUp,   } from "lucide-react";
+import { Clock, Landmark, Moon, Sun, TrendingUp,   } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
+import { useTheme } from "../../hooks/useTheme";
+import { Divider } from "./Divider";
+
+
 
 export function Header() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
+  const {theme, toggleTheme} = useTheme()
   return (
     <header className="border-b border-(--border) px-6 py-3">
       <nav className="flex items-center justify-between">
@@ -13,8 +18,8 @@ export function Header() {
           </div>
 
           <span className="text-lg">
-            <span className="text-muted-foreground font-medium">Planej</span>
-            <span className="font-extrabold">.ai</span>
+            <span className="text-muted-foreground font-medium">Finance</span>
+            <span className="font-extrabold">.Ai</span>
           </span>
         </div>
         <div className="flex items-center gap-1">
@@ -25,13 +30,21 @@ export function Header() {
           >
             <span className="hidden sm:inline">Nova Simulação</span>
           </Button>
-            <Button
-            variant="ghost"
-            icon={Clock}
-            onClick={() => void navigate('/historico')}
-          >
-            <span className="hidden sm:inline">Histórico</span>
-          </Button>
+           <Button
+  variant="ghost"
+  icon={Clock}
+  onClick={() => void navigate('/historico')}
+>
+  <span className="hidden sm:inline">Histórico</span>
+</Button>
+<Divider orientation="vertical"/>
+<Button
+  aria-label={`Mudar para tema ${theme === 'light' ? 'escuro' : 'claro'}`}
+  variant="ghost"
+  icon={theme === 'light' ? Moon : Sun}
+  onClick={toggleTheme}
+/>
+         
           </div>
       </nav>
     </header>
